5083ec32c5e7a849b0d5e6cf328af3d0539a8adb
kryojenik <kryojenik2@gmail.com>
2026-01-18 15:26:21 -0600

    Move stack loaders to data-updates.  Need to have the py-* technologies available first.

--- data-updates.lua
+++ data-updates.lua
@@ -1,3 +1,5 @@
+require("prototypes.py")
+
 if mods["pycoalprocessing"] and settings.startup["mdrn-enable-stacking"].value == "stack-tier" then
   local stack_tech = data.raw["technology"]["stack-mdrn-loader"]
   local prereq = data.raw["technology"][stack_tech.prerequisites[1]]
