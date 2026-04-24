4ba76d81c047d1b0ade681809755e0199c11c783
kryojenik <kryojenik2@gmail.com>
2026-04-24 00:41:39 -0500

    Locale updates and version bump to 2.0.0

--- prototypes/krastorio2-ff.lua
+++ prototypes/krastorio2-ff.lua
@@ -1,20 +1,21 @@
 -- If there is another mod that enables belt stacking and stack loaders in Krastorio 2
 -- decide if we need to set up a stack loader.
-local startup_settings = settings.startup
-if startup_settings["mdrn-enable-stacking"].value == "stack-tier" then
+local C   = require("__loaders-modernized__.constants")
+local cfg = require("__loaders-modernized__.prototypes.settings-cache")
+if cfg.stacking == C.STACKING.STACK_TIER then
   local loaders = {}
   -- TODO: This should probably move to the SE mod.
   if not mods["space-exploration"] then
-    loaders["superior-"] = { next_upgrade = "stack-mdrn-loader" }
+    loaders["superior-"] = { next_upgrade = "mdrn-stack-loader" }
   end
   local superior_inserter = data.raw["inserter"]["kr-superior-inserter"]
-  if startup_settings["mdrn-use-k2-recipes"].value and superior_inserter.max_belt_stack_size and superior_inserter.max_belt_stack_size > 1 then
+  if settings.startup["mdrn-use-k2-recipes"].value and superior_inserter.max_belt_stack_size and superior_inserter.max_belt_stack_size > 1 then
     loaders["stack-"] = {
       recipe_data = {
         ingredients = {
           { type = "item", name = "processing-unit", amount = 1 },
           { type = "item", name = "kr-superior-inserter", amount = 6 },
-          { type = "item", name = "fast-mdrn-loader", amount = 1 },
+          { type = "item", name = "mdrn-fast-loader", amount = 1 },
         }
       }
     }
--- prototypes/krastorio2.lua
+++ prototypes/krastorio2.lua
@@ -1,16 +1,16 @@
 -- Define the loader template for the tier 5 turbo belt
 local loaders = {
   ["advanced-"] = {
-    upgrade_from_prefix = "express-",
+    upgrade_from_tier = "express-",
     next_upgrade = "mdrn-superior-loader",
     underground_name = "kr-advanced-underground-belt",
     order = "05",
     tint = util.color("4fbf38d1"),                                 -- Green tint for Advanced tier
-    prerequisite_techs = { "kr-logistic-4", "express-mdrn-loader" }, -- Link to logistic-4 technology
+    prerequisite_techs = { "kr-logistic-4", "mdrn-express-loader" }, -- Link to logistic-4 technology
     recipe_data = {
       ingredients = {
         { type = "item", name = "kr-advanced-underground-belt", amount = 1 },
-        { type = "item", name = "express-mdrn-loader",      amount = 1 },
+        { type = "item", name = "mdrn-express-loader",      amount = 1 },
         { type = "item", name = "bulk-inserter",          amount = 4 }
       }
     }
@@ -19,11 +19,11 @@ local loaders = {
     underground_name = "kr-superior-underground-belt",
     order = "06",
     tint = util.color("862f9fd1"),                                  -- Purple tint for superior tier
-    prerequisite_techs = { "kr-logistic-5", "advanced-mdrn-loader" }, -- Link to logistic-5 technology
+    prerequisite_techs = { "kr-logistic-5", "mdrn-advanced-loader" }, -- Link to logistic-5 technology
     recipe_data = {
       ingredients = {
         { type = "item", name = "kr-superior-underground-belt", amount = 1 },
-        { type = "item", name = "advanced-mdrn-loader",      amount = 1 },
+        { type = "item", name = "mdrn-advanced-loader",      amount = 1 },
         { type = "item", name = "kr-superior-inserter",          amount = 2 }
       }
     }
@@ -35,12 +35,12 @@ if settings.startup["mdrn-use-k2-recipes"].value == true then
   loaders["advanced-"].recipe_data.ingredients = {
     { type = "item", name = "kr-rare-metals", amount = 10 },
     { type = "item", name = "kr-advanced-transport-belt", amount = 1 },
-    { type = "item", name = "express-mdrn-loader", amount = 2 },
+    { type = "item", name = "mdrn-express-loader", amount = 2 },
   }
   loaders["superior-"].recipe_data.ingredients = {
     { type = "item", name = "kr-imersium-gear-wheel", amount = 10 },
     { type = "item", name = "kr-superior-transport-belt", amount = 1 },
-    { type = "item", name = "advanced-mdrn-loader", amount = 2 },
+    { type = "item", name = "mdrn-advanced-loader", amount = 2 },
   }
 
   -- Create new template entries to modify already existing loaders from base.
@@ -67,7 +67,7 @@ if settings.startup["mdrn-use-k2-recipes"].value == true then
       ingredients = {
         { type = "item", name = "kr-steel-gear-wheel", amount = 10 },
         { type = "item", name = "express-transport-belt", amount = 1 },
-        { type = "item", name = "fast-mdrn-loader", amount = 2 },
+        { type = "item", name = "mdrn-fast-loader", amount = 2 },
       }
     }
   }
