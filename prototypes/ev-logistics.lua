d8cb138b5b1f49529f65f16fc4072ac06af98b36
kryojenik <kryojenik2@gmail.com>
2026-04-24 00:41:14 -0500

    Locale updates and version bump to 2.0.0

--- prototypes/ev-logistics.lua
+++ prototypes/ev-logistics.lua
@@ -3,24 +3,27 @@ if not settings.startup["ev-belts-enabled"].value then
   return false
 end
 
+local C   = require("__loaders-modernized__.constants")
+local cfg = require("__loaders-modernized__.prototypes.settings-cache")
+
 -- Define the loader template for the tier 5 turbo belt
 local loaders = {
   ["hyper-"] = {
-    upgrade_from_prefix = "turbo-",
+    upgrade_from_tier = "turbo-",
     tint = util.color("8c48dbd1"),                               -- Purple tint for hyper tier
-    prerequisite_techs = { "hyper-logistics", "turbo-mdrn-loader" }, -- Link to hyper-logistics technology
+    prerequisite_techs = { "hyper-logistics", "mdrn-turbo-loader" }, -- Link to hyper-logistics technology
     recipe_data = {
       ingredients = {
         { type = "item", name = "hyper-underground-belt", amount = 1 },
-        { type = "item", name = "turbo-mdrn-loader",      amount = 1 },
+        { type = "item", name = "mdrn-turbo-loader",      amount = 1 },
         { type = "item", name = "bulk-inserter",          amount = 3 }
       }
     }
   }
 }
 
-if settings.startup["mdrn-enable-stacking"].value == "stack-tier" then
-  loaders["hyper-"].next_upgrade = "stack-mdrn-loader"
+if cfg.stacking == C.STACKING.STACK_TIER then
+  loaders["hyper-"].next_upgrade = "mdrn-stack-loader"
 end
 
 MdrnLoaders.add_loaders(loaders)
