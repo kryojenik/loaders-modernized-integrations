3393995cda936290571fd436c246ae5596a67b78
kryojenik <kryojenik2@gmail.com>
2026-05-01 10:31:05 -0500

    fix missing stack

--- data-updates.lua
+++ data-updates.lua
@@ -4,6 +4,8 @@ local cfg = require("__loaders-modernized__.prototypes.settings-cache")
 
 if mods["pycoalprocessing"] and cfg.stacking == C.STACKING.STACK_TIER then
   local stack_tech = data.raw["technology"]["mdrn-stack-loader"]
+  if not stack_tech then return end
+
   local prereq = data.raw["technology"][stack_tech.prerequisites[1]]
   if not prereq then
     data.raw["technology"]["mdrn-stack-loader"] = nil
