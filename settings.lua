747f0ceaf52e5a28a2de793ebd71115699d6785e
kryojenik <kryojenik2@gmail.com>
2026-05-04 19:29:03 -0500

    Fix 5Dim details I messed up.

--- settings.lua
+++ settings.lua
@@ -9,3 +9,16 @@ if mods["Krastorio2"] then
     }
   })
 end
+
+if mods["5dim_transport"] then
+  data:extend({
+    {
+      type = "string-setting",
+      name = "mdrn-keep-5d-loaders",
+      order = "sg",
+      setting_type = "startup",
+      default_value = "none",
+      allowed_values = {"none", "1x2", "all"}
+    },
+  })
+end
