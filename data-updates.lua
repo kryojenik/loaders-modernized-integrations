ec1eb93771d34025ab94fe53387a0f2e2df88a97
kryojenik <kryojenik2@gmail.com>
2026-05-07 00:29:57 -0500

    Fixed py not loading
    During the merge of mods I moved the stack loader creation into data.  It needs to remain in data-updates because Pyanodon's doesn't create stack inserters until data-updates.

--- data-updates.lua
+++ data-updates.lua
@@ -1,3 +1,4 @@
+-- Pyanadons adds the stack loader in data-updates.  We need to wait until now to create a stack loader.
 require("prototypes.py.py-updates")
 require("prototypes.se.krastorio2-compatibility")
 require("prototypes.ubsa.ubsa-updates")
--- prototypes/py/py-updates.lua
+++ prototypes/py/py-updates.lua
@@ -3,15 +3,23 @@ if not mods["pycoalprocessing"] then return end
 local C   = require("__loaders-modernized__.constants")
 local cfg = require("__loaders-modernized__.prototypes.settings-cache")
 
-if cfg.stacking == C.STACKING.STACK_TIER then
-  local stack_tech = data.raw["technology"]["mdrn-stack-loader"]
-  if not stack_tech then return end
-
-  local prereq = data.raw["technology"][stack_tech.prerequisites[1]]
-  if not prereq then
-    data.raw["technology"]["mdrn-stack-loader"] = nil
-    return
-  end
-
-  stack_tech.unit = prereq.unit
+if cfg.stacking == C.STACKING.STACK_TIER
+and data.raw["inserter"]["py-stack-inserter"] then
+  MdrnLoaders.add_loaders{
+    ["stack-"] = {
+      order = "z",
+      tint = util.color("F5F5F5D1"),
+      underground_name = "express-underground-belt",
+      upgrade_from_tier = "express-",
+      prerequisite_techs = { "py-transport-belt-capacity-1", "mdrn-express-loader" },
+      max_belt_stack_size = data.raw["utility-constants"].default.max_belt_stack_size,
+      recipe_data = {
+        ingredients = {
+          --{type = "item", name = "express-underground-belt", amount = 1},
+          {type = "item", name = "py-stack-inserter", amount = 3},
+          {type = "item", name = "mdrn-express-loader", amount = 1},
+        },
+      }
+    },
+  }
 end
