c7082247c583b5cd161ff27d5885ca0e469fedbc
kryojenik <kryojenik2@gmail.com>
2026-05-08 14:13:41 -0500

    Moves the stack loader to depend on fast loaders instead of express loaders.
    Fits the py progression better and fits in with stack inserters.
    Fixes: kryojenik/loaders-modernized-py#1

--- prototypes/py/py-updates.lua
+++ prototypes/py/py-updates.lua
@@ -11,13 +11,13 @@ and data.raw["inserter"]["py-stack-inserter"] then
       tint = util.color("F5F5F5D1"),
       underground_name = "express-underground-belt",
       upgrade_from_tier = "express-",
-      prerequisite_techs = { "py-transport-belt-capacity-1", "mdrn-express-loader" },
+      prerequisite_techs = { "py-transport-belt-capacity-1", "mdrn-fast-loader" },
       max_belt_stack_size = data.raw["utility-constants"].default.max_belt_stack_size,
       recipe_data = {
         ingredients = {
           --{type = "item", name = "express-underground-belt", amount = 1},
           {type = "item", name = "py-stack-inserter", amount = 3},
-          {type = "item", name = "mdrn-express-loader", amount = 1},
+          {type = "item", name = "mdrn-fast-loader", amount = 1},
         },
       }
     },
